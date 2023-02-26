2c2a7da4e72772ad67466d6f4e8d8b58146f57b7
Radbuglet <10578060+Radbuglet@users.noreply.github.com>
2023-02-25 16:34:03 -0800

    Finish animating section 5 scene 2

--- src/scenes/sect_5_scn_2_bob_resp.tsx
+++ src/scenes/sect_5_scn_2_bob_resp.tsx
@@ -1,11 +1,13 @@
 import { makeScene2D } from "@motion-canvas/2d";
-import { Latex, Text } from "@motion-canvas/2d/lib/components";
-import { waitUntil } from "@motion-canvas/core/lib/flow";
+import { Node, Rect, Latex, Text, Circle } from "@motion-canvas/2d/lib/components";
+import { waitFor, waitUntil } from "@motion-canvas/core/lib/flow";
 import { easeInExpo, easeInOutCubic, easeOutExpo } from "@motion-canvas/core/lib/tweening";
 import { Vector2 } from "@motion-canvas/core/lib/types";
 import { createRef } from "@motion-canvas/core/lib/utils";
+import { Graph } from "../components/graph";
 import { animateSpawn, dropOut, growIn, growOutTo } from "../components/animations";
 import { Bob } from "../components/characters";
+import { animateParticles, DEFAULT_PARTICLE_CONFIG, differentialSimulator, Particle } from "../components/flow";
 
 export default makeScene2D(function* (view) {
 	const title = createRef<Text>();
@@ -90,8 +92,86 @@ export default makeScene2D(function* (view) {
 		growOutTo(new Vector2(400, 0), 1),
 	);
 
-	// Bob's secret weapon
-	// TODO
+	// Bob's secret weapon...?
+	const board = createRef<Graph>();
+	const line_layer = createRef<Node>();
+	const accept_layer = createRef<Node>();
+	const exception = createRef<Circle>();
+
+	yield* waitUntil("bob's weapon");
+	yield shrink_fx().position(shrink_fx().position().addY(-300), 1, easeOutExpo);
+	yield eq_3().position(eq_3().position().addY(-250), 1, easeOutExpo);
+	yield eq_4().position(eq_4().position().addY(-250), 1, easeOutExpo);
+	yield animateSpawn(
+		view,
+		<Graph
+			ref={board}
+			y={100}
+			width={750}
+			height={750}
+		>
+			<Node ref={line_layer}>
+				<Rect x={0} y={0} width={0.1} height={100} fill="black" />
+				<Rect x={0} y={0} width={100} height={0.1} fill="black" />
+				<Rect x={4} y={0} width={0.1} height={100} fill="red" />
+			</Node>
+			<Node ref={accept_layer}>
+				{[4, 3, 5, 7, 1, -2].map(v => <Particle
+					x={-10}
+					y={-v - 3}
+					max_trail_length={200}
+					color={v === 4 ? "blue" : "darkblue"}
+				/>)}
+				<Circle ref={exception} x={4} y={-4} width={0.5} height={0.5} fill="darkred" />
+			</Node>
+		</Graph>,
+		growIn(),
+	);
+
+	eq_4().tex(() => `y = \\color{darkred}\\text{${-Math.round((4 + accept_layer().position().y) * 100) / 100}}`);
+
+	for (const particle of accept_layer().children()) {
+		if (!(particle instanceof Particle)) continue;
+
+		const ty = particle.position().y + 3;
+		yield animateParticles(
+			particle,
+			differentialSimulator(({ x, y }) => - (y - ty) / 2),
+			(Math.abs(ty - (-4)) < 0.1) ? 20 : 14,
+			2,
+			DEFAULT_PARTICLE_CONFIG,
+			easeOutExpo,
+		);
+	}
+
+	yield* waitFor(2);
+	for (const particle of accept_layer().children()) {
+		if (!(particle instanceof Particle)) continue;
+
+		if (particle.position().x < 5) {
+			yield animateSpawn(
+				particle,
+				<Circle width={0.75} height={0.75} lineWidth={0.1} stroke="black" />,
+				growIn(),
+			);
+		}
+	}
+
+	// Move the points around
+	yield* waitUntil("move around");
+	yield* accept_layer().position(accept_layer().position().addY(-3), 2, easeInOutCubic);
+	yield* accept_layer().position(accept_layer().position().addY(5), 2, easeInOutCubic);
+
+	// Is this enough?
+	yield* waitUntil("is this enough");
+	yield board().scale(0.75, 1, easeOutExpo);
+	yield* animateSpawn(
+		view,
+		<Node y={50}>
+			<Text fontSize={140}>Is this enough?</Text>
+		</Node>,
+		growIn(),
+	);
 
 	yield* waitUntil("scene end");
 });
